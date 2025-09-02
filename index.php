4c297063b3e1164c6a5bf65f013f2bf0ca86fe89
𝙻𝚞𝚜𝚔𝚊𝚜 <lucas.bombonato6@gmail.com>
2025-09-02 16:42:54 -0300

    primeira parte

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+    $nomes = ["Maria", "Fábio", "Caio"];
+    $nomes[] = "Ana";
+    $nomes[2] = "Carlos";
+
+    rsort($nomes);
+
+    echo "Segundo nome: " . $nomes[1];
+
+    for ($c = 0, $c < count($nomes); $c++;) {
+        echo "<br>Nomes " . $c . ": " . $nomes[$c];
+    }
+
+    $idades = array(25, 36, 15, "dez");
+
+    echo "<br>Última idade: " . $idades[3];
+    echo "<br>Total de idades: " . count($idades);
+
+    $aluno = [
+        "matrícula" => 123,
+        "nome" => "Júlio",
+        "curso" => "DSM",
+    ];
+
+    echo "<br>Nome: " . $aluno['nome'];
+    $aluno["período"] = "Vespertino";
+
+    foreach ($aluno as $a) {
+        echo "<br>$a";
+    }
+
+    $unidades = [
+        0 => "PG",
+        1 => "SV"
+    ];
+
+    foreach ($unidades as $chave => $valor) {
+        echo "<br>$chave : $valor";
+    }
+?>
---
+++ select.php
@@ -0,0 +1,26 @@
+<?php 
+      $cursos = [
+            "ADS"   => "Análise e Desenvolvimento de Sistemas",
+            "COMEX" => "Comércio Exterior",
+            "DSM"   => "Desenvolvimento de Software Multiplataforma",
+            "GE"    => "Gestão Empresarial",
+            "PQ"    => "Processos Químicos",
+      ];
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Select</title>
+</head>
+<body>
+      <form action="#" method="post">
+            <label>Curso:</label>
+            <select name="sltcurso">
+
+            </select>
+      </form>
+</body>
+</html>
