75b357dcad5ab56f0a94a86a2958919ff51e60ef
𝙻𝚞𝚜𝚔𝚊𝚜 <lucas.bombonato6@gmail.com>
2025-09-02 18:17:40 -0300

    adicionando galeria

--- select.php
+++ select.php
@@ -2,6 +2,11 @@
       $usuarios = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"];
       $cursos = ["ADS","COMEX","DSM","GE","PQ"];
       $periodos = ["Matutino", "Vespertino", "Noturno"];
+      $fotos = [
+            "https://images.squarespace-cdn.com/content/v1/606d159a953867291018f801/1619987265163-9XILMVT3TK4HZ5X6538M/VH_01_1080pjpg.jpg",
+            "https://th.bing.com/th/id/R.f1a83630b91b3f128e8f9dd58f3f8eed?rik=jkPZWEeSAZbPjQ&pid=ImgRaw&r=0",
+            "https://th.bing.com/th/id/R.f5c52749d681a49d981200c3dadcf5b4?rik=y4kgyCqpGua8Uw&pid=ImgRaw&r=0"
+      ];
 ?>
 
 <!DOCTYPE html>
@@ -40,6 +45,14 @@
             ?>
             <br><br>
             <input type="submit" value="Enviar">
+
+            <br>
+            <h2>Galeria</h2>
+            <?php
+                  foreach($fotos as $f){
+                        echo "<img src=$f width='200px' style='margin:10px; border-radius:10px;'>";
+                  }
+            ?>
       </form>
 
       <?php 
